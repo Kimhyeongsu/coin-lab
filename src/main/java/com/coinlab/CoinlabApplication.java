c2ad93c697f4507603b65f1252c034371a075bfd
Kimhyeongsu <khs0324@gmail.com>
2018-02-18 21:01:52 +0900

    * 1st Commit

---
+++ src/main/java/com/coinlab/CoinlabApplication.java
@@ -0,0 +1,12 @@
+package com.coinlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinlabApplication.class, args);
+	}
+}
